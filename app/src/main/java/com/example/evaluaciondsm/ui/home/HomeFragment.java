4e3df8907eade456f01dc9a440ad9091db4b7397
rickyurvinaUCN <ricardo.urvina@alumnos.ucn.cl>
2023-11-09 17:47:06 -0300

    Add Class AdminSQLiteOpenHelper

--- app/src/main/java/com/example/evaluaciondsm/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/evaluaciondsm/ui/home/HomeFragment.java
@@ -1,10 +1,14 @@
 package com.example.evaluaciondsm.ui.home;
 
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -18,11 +22,37 @@ public class HomeFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        HomeViewModel homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
 
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
+        binding.btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                save();
+            }
+        });
+
+        binding.btnSearchP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                search();
+            }
+        });
+
+        binding.btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                delete();
+            }
+        });
+
+        binding.btnEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                edit();
+            }
+        });
 
         return root;
     }
@@ -32,4 +62,26 @@ public class HomeFragment extends Fragment {
         super.onDestroyView();
         binding = null;
     }
+
+    //Metodo para guardar los productos
+    public void save() {
+
+    }
+
+    //Metodo para buscar productos
+    public void search() {
+
+    }
+
+    public void delete() {
+
+    }
+
+    public void edit() {
+
+    }
+
+    public void cleanForm() {
+    }
+
 }
